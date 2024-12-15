66926d69ffd22e73e1c59106d0b95aeba6249e12
Nischal Kumar Shrestha <150148730+nischalkshrestha@users.noreply.github.com>
2024-12-15 15:14:34 +0545

    Delete Week 3 Programming Portfolio directory

--- Week 3 Programming Portfolio/1 - Ans.py
+++
@@ -1,9 +0,0 @@
-"""1. Modify your greeting program so that if the user does not enter a name (i.e. they
-just press enter), the program responds "Hello, Stranger!". Otherwise it should print
-a greeting with their name as before."""
-
-name = input("Hello, what is your name? ")
-if name:
-    print("Hello,",name,". Good to meet you!")
-else:
-    print("Hello Stranger!")    
