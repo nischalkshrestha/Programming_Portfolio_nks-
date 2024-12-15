30f5b2a6a8d0aa65a2676cd2f8c984b2309b936d
Nischal Kumar Shrestha <150148730+nischalkshrestha@users.noreply.github.com>
2024-12-15 15:14:20 +0545

    Delete Week 2 Programming Portfolio directory

--- Week 2 Programming Portfolio/2 - Ans.py
+++
@@ -1,8 +0,0 @@
-"""2. Write a program that prompts a user to enter a temperature in Celsius, and then
-displays the corresponding temperature in Fahrenheit, like so:
-Enter a temperature in Celsius: 32.5
-32.5C is equivalent to 90.5F."""
-
-celsius = float(input("Enter a temperature in Celsius: "))
-fahreinheit = (celsius * 9 / 5) + 32
-print(celsius,"C is equivalent to",fahreinheit,"F")
